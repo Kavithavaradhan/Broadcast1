9c370a4076aeaba775af1874f2549ab61b2b122a
kavi <kavithav0509@gmail.com>
2018-07-12 18:47:30 +0530

    First Commit

--- app/src/main/java/com/kavi/broadcast1/InterceptCall.java
+++ app/src/main/java/com/kavi/broadcast1/InterceptCall.java
@@ -3,6 +3,9 @@ package com.kavi.broadcast1;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.provider.Telephony;
+import android.telephony.TelephonyManager;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 /**
@@ -12,8 +15,27 @@ import android.widget.Toast;
 public class InterceptCall extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+
         try {
-            Toast.makeText(context,"Ringing!",Toast.LENGTH_SHORT).show();
+                String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+            if (state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_RINGING)) {
+               Toast.makeText(context,"Ringing!",Toast.LENGTH_SHORT).show();
+
+
+                //PopupMenu popup = new PopupMenu(InterceptCall.this,);
+
+                //popup.getMenuInflater().inflate(R.menu.popupmenu,popup.getMenu());
+
+            }
+            if (state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
+                Toast.makeText(context,"Received",Toast.LENGTH_SHORT).show();
+
+            }
+
+            if (state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_IDLE)) {
+                Toast.makeText(context,"Idle",Toast.LENGTH_SHORT).show();
+
+            }
         } catch (Exception e){
             e.printStackTrace();
         }
